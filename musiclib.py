d28cfd31f873918d5f1bd9fb583f14548df5d71d
AjayHyati <134322519+AjayHyati@users.noreply.github.com>
2025-05-10 23:52:17 +0530

    Add files via upload

---
+++ musiclib.py
@@ -0,0 +1,6 @@
+music={
+    "yenagali":"https://www.youtube.com/watch?v=twgEwLoswcs&pp=ygUNa2FubmFkYSBzb25ncw%3D%3D",
+    "salaga":"https://www.youtube.com/watch?v=anSLNvs27WY&pp=ygUNa2FubmFkYSBzb25ncw%3D%3D",
+    "dwapara":"https://www.youtube.com/watch?v=wiur_AGatGU&pp=ygUNa2FubmFkYSBzb25ncw%3D%3D"
+
+}
